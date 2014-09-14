aa81a279f513aad91d2a2d88959033e17ed3b541
John Zhang <john.shimin.zhang@gmail.com>
2014-09-14 15:01:33 -0400

    allow human player and comptuer pick word

--- hangman.rb
+++ hangman.rb
@@ -1,32 +1,60 @@
 class Hangman
-  attr_accessor :board, :turns
+  attr_accessor :board, :turns, :prosecutor, :defendant
 
-  def initialize(turns = 10)
+  def initialize(prosecutor, defendant, turns = 10)
     self.turns = 10
+    self.prosecutor = prosecutor
+    self.defendant = defendant
   end
 
   def play
+    puts "Welcome to the game of hangman, where the prisoner may escape punishment but the system stays unchanged, always, just the way it's meant to be. \n \n"
+    #Get A Secret Word From Prosecutor And Updates Board
+    board_size = prosecutor.make_secret
+    self.board = Array.new(board_size,"_")
 
-    #Get A Secret Word From Prosecutor
 
-    #Prosecutor Tells Defendent How Many Letters In The Word
-
+    #Game Tells Defendent How Many Letters In The Word
+    defendant.get_secrete_hint(board_size)
+    puts " 'The Code is #{board_size} letters long.' The Prosecutor says with a blank expression on her face. What could it possibly be?? \n \n"
     #Print The Board
+    render
 
     #While Game Is Not Over
+    while self.turns > 0 && self.board.select{|l| l == "_"}.length != 0
 
-    #Get Letter From Defendent
+      #Get Letter From Defendent
+      guess = defendant.guess_letter
+      puts "'And the defendant guesses #{guess.upcase}!!' The jury murmurs amongst themselves."
+      # Prosecutor Tells Defendent If It's Correct
+      pos = prosecutor.judge_guess(guess)
+      pos.each do |p|
+        self.board[p] = guess
+      end
+      if pos.count == 0
+        render
+        puts "The prisoner walks one step closer to the gallows, who would've thought #{guess} was a good idea? \nGood job defendant! \n\n"
+      else
+        render
+        puts "Looks like the defendant was able to scrap by with some questionable evidence. \nPrisoner, feel free to take another breathe, enjoy it.\n\n"
+      end
+      # One Step Closer To Death
+      self.turns -= 1
 
-    # Prosecutor Tells Defendent If It's Correct
-
-    # One Step Closer To Death
-
-    # Prisoner Is Dead If Turns Run Out
-
+    end
+    # Defendent Is Dead If Turns Run Out
+    if self.turns == 0
+      puts "'Thump!' The prisoner's boots have finally reached the steps leading toward the gallows.\nHis face is as white as the knuckles of his interrogator last night (before all the blood of course).\nAnd another quick victory for the prosecutor, a glorious example of how the legal system should work.\n\nLook, the beautiful murder of ravens are already circling above!\n\n"
+    end
     # Defendent Wins
-
+    if self.board.select{|l| l == "_"}.length == 0
+      puts "The defendant snatches another victim from the jaws of the law. \nNext time, he wouldn't be so lucky.\n\n"
+    end
   end
 
+  def render
+    puts self.board.join("") + "\n"
+  end
 end
 
 class Player
@@ -51,3 +79,59 @@ class Player
     #process result of prosecutor's judgment (computer only)
   end
 end
+
+class ComputerPlayer
+  attr_accessor :dictionary, :secret_code
+
+  def initialize(file_name)
+    self.dictionary = File.readlines(file_name).map(&:chomp)
+  end
+
+  def make_secret
+    # self.secret_code = self.dictionary.sample
+    self.secret_code = 'cat'
+    self.secret_code.length
+  end
+
+  def judge_guess(letter)
+    pos = []
+    self.secret_code.split('').each_with_index {|n,i| pos << i if n == letter}
+    pos
+  end
+
+end
+
+class HumanPlayer
+  attr_accessor :letters_remain
+  def initialize
+    self.letters_remain = ("a".."z").to_a
+  end
+
+  def get_secrete_hint(num)
+
+  end
+
+  def guess_letter
+    puts "You open your left hand and see the letters #{letters_remain.join(' ')}, which one will you pick next? \n\n"
+    letter = gets.chomp
+    unless self.letters_remain.include?(letter)
+      "You've already used #{letter}, looks like you aren't very good at this thing. \n\nWorry not, we know not of second chances in these parts of the world\n\n"
+    end
+    self.letters_remain.delete(letter)
+    puts "'#{letter.upcase}' You said, unsure if this is the one that will seal the prisoner's fate.\n\n"
+    letter
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+
+  computer = ComputerPlayer.new('dictionary.txt')
+  human = HumanPlayer.new
+  game = Hangman.new(computer, human)
+  game.play
+end
+
+
+
+
+
